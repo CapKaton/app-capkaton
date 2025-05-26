9292d26b7159a7af69549986c9e6cd5e55e862ff
Estelar IX <layannyoliveira01@gmail.com>
2025-05-25 21:51:13 -0300

    Construindo tela de cadastro

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Home from "../components/Home";
+
+
+export default function Homee() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-[#0f111a]">
+      <Home />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import Header from "./components/Login";
-import Home from "./components/Home";
+import Login from "./components/Login";
+import Link from "next/link";
+import Image from 'next/image';
+
 
 
 export default function Page() {
   return (
-    <div className="aling-center justify-center ">
-      
-      <Home />
-      
-      {/* 
-      <Login />
-      <Footer /> */}
-    </div>
+    <main className="flex flex-row justify-around items-center bg-[#0f111a] min-h-screen">
+      <div className="p-4 flex content-around items-center">
+        <Image
+          src="/logo.png"
+          alt="Logo do Hackathon"
+          width={660}
+          height={330}
+          />
+      </div>
+      <div className="flex flex-row justify-end items-center">
+        <Login />
+      </div>
+    </main>
   );
 }
